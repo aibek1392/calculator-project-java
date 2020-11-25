b9161b4e423c9bdd8c47357f1888c7d35c8e1187
aibek <ozhorov@gmail.com>
2020-11-24 22:24:13 -0500

    first

---
+++ src/calculatorproject/Inputs.java
@@ -0,0 +1,15 @@
+package calculatorproject;
+
+public class Inputs {
+	double a, b;
+	byte choice;
+	public double setA(double a) {
+		return this.a = a;
+	}
+	public double setB(double b) {
+		return this.b = b;
+	}
+	public byte setChoice(byte choice) {
+		return this.choice = choice;
+	}
+}
